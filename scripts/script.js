e1ba5b8210d7981cf8ba6c64c21243c2ea527bba
DariaRadu <dariaradur3@gmail.com>
2017-11-14 22:42:56 +0100

    minor changes

--- scripts/script.js
+++ scripts/script.js
@@ -212,3 +212,171 @@ tempSec3.addEventListener('click', function()
     seriesChart.data.datasets[4].data=[108, 109, 145, 136, 127, 139, 158];
     seriesChart.update();
 });
+
+//NEW SCRIPT
+var pages=document.getElementsByClassName("page");
+var currentPage=0;
+loginForm.addEventListener("keypress",function(e)
+{
+	if(e.keyCode==13)
+	{
+		login();
+	}
+})
+btnLogin.addEventListener("click",login);
+function login()
+{
+	if(loginForm.user.value=="admin"&&loginForm.pass.value=="pass")
+	{
+		pages[currentPage].classList.remove("pageActive");
+		currentPage=1;
+		pages[currentPage].classList.add("pageActive");
+	}
+	else
+	{
+		console.log("Error");
+	}
+}
+
+btnSettings.addEventListener("click",openSettings);
+function openSettings()
+{	
+	document.getElementsByClassName("back")[0].classList.add("backActive");
+
+}
+var icons=document.getElementsByTagName("i");
+for (var i=0;i<icons.length;i++)
+{
+	icons[i].addEventListener("click",iconClick);
+}
+function iconClick(e)
+{
+		var icon=e.target;
+		var el=e.target.parentElement.getElementsByTagName("span")[0];
+		icon.classList.remove("fa-pencil");
+		icon.classList.add("fa-times");
+		icon.dataset.ex=el.innerHTML;
+		icon.removeEventListener("click",iconClick);
+		el.contentEditable=true;
+		el.focus();
+		document.secretPar=icon;
+		setTimeout(function(){document.addEventListener("click",save)},1000);
+		el.style.backgroundColor="white";
+		icon.addEventListener("click",iconCancel);
+
+}
+function save(e)
+{
+	document.secretPar.parentElement.getElementsByTagName("span")[0].contentEditable=false;
+	document.secretPar.parentElement.getElementsByTagName("span")[0].style.backgroundColor="rgba(0,0,0,0)";
+	document.secretPar.classList.remove("fa-times");
+	document.secretPar.classList.add("fa-pencil");
+	document.removeEventListener("click",save);
+	document.secretPar.dataset.ex="";
+}
+function iconCancel(e)
+{
+	document.removeEventListener("click",save);
+	e.target.classList.remove("fa-times");
+	e.target.classList.add("fa-pencil");
+	var el2=e.target.parentElement.getElementsByTagName("span")[0];
+	if(e.target.dataset.ex)
+	el2.innerHTML=e.target.dataset.ex;
+	el2.contentEditable=false;
+	el2.style.backgroundColor="rgba(0,0,0,0)";
+	e.target.addEventListener("click",iconClick);
+	e.target.removeEventListener("click",iconCancel);
+}
+closeIcon.addEventListener("click",function()
+{
+	document.getElementsByClassName("backActive")[0].classList.remove("backActive");
+});
+
+//SEARCH BAR DUMMY
+var counter = 0;
+var value = "";
+
+var blueprintSearch = 
+'<li><div class="result-label" style="background-color: {{color}}">\
+    {{letter}}\
+</div>\
+<div class="result-text">\
+    <a href="">{{name}}</a>\
+    <a href="">{{title}}</a>\
+</div></li>';
+
+
+var ajResult = [
+{	"letter":"A",
+    "color":"red",
+    "name":"Adam Hegedus",
+    "title":"Filming and Photography"},
+{	"letter":"L",
+    "color":"teal",
+    "name":"Lynn P. Asmussen",
+    "title":"Drawing circles"},
+{	"letter":"C",
+    "color":"#222",
+    "name":"Claudiu C.",
+    "title":"Coding C# faster than you"},
+{	"letter":"D",
+    "color":"pink",
+    "name":"Daria Darling",
+    "title":"Doing the deed of doodleling"},
+{	"letter":"L",
+    "color":"darkgreen",
+    "name":"Line Hjort K.",
+    "title":"Maker of green kale chips"}
+];
+
+$('#form-searchbar input').on('input', search(this.value));
+
+function search(value)
+{
+    $('#textSpan').html(value);
+    counter=value.length;
+    showResult(counter)
+    if(counter > 0)
+    {
+        $('#search-box').css('display','flex');	
+    }
+    else
+    {
+        $('#search-box').hide();
+    }
+}
+
+var name ="";
+var title ="";
+var letter ="";
+var color ="";
+var result ="";
+
+function showResult(number){
+    $('#search-list').html("");
+
+    for(var i = 0 ; i<ajResult.length; i++){
+
+        letter=ajResult[i].letter;
+        color=ajResult[i].color;
+        name=ajResult[i].name;
+        title=ajResult[i].title;
+
+        result = blueprintSearch.replace("{{letter}}",letter);
+        result = result.replace("{{name}}",name);
+        result = result.replace("{{title}}",title);
+        result = result.replace("{{color}}",color);
+        
+        $('#search-list').append(result);
+    }
+}
+
+//make the search disappear by clicks, if there is no content
+var inputContent = "";
+document.addEventListener("click", function()
+{
+    inputContent = $('#form-searchbar input').val();
+    if (inputContent == ""){
+        $('#search-box').hide();
+    }
+});
