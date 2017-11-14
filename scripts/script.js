8b893deb7cf60fe19534eaff0146593deabae0c6
Clauzzz <claudiu.cuciurean@yahoo.com>
2017-11-14 14:26:30 +0100

    New commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
 //CLICK RATE CHART
 var ctx = document.getElementById('clickRateChart').getContext('2d');
-var clickRateChart = new Chart(ctx, {
+var clickRateChart = new Chart(ctx, 
+{
     // The type of chart we want to create
     type: 'line',
 
     // The data for our dataset
     data: {
         labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-        datasets: [{
+        datasets: 
+        [{
             label: "Click Rate",
             borderColor: '#004977',
             backgroundColor:'#004977',
@@ -19,66 +21,79 @@ var clickRateChart = new Chart(ctx, {
     },
 
     // Configuration options go here
-    options: {
-        scales : {
-        xAxes : [ {
-            gridLines : {
-                display : false
-            }
-        } ]
-    }
+    options: 
+    {
+        scales : 
+        {
+            xAxes : 
+            [{
+                gridLines : 
+                {
+                    display : false
+                }
+            }]
+        }
     }
 });
 
 //CHANGING BETWEEN DAY/WEEK/MONTH
 var weekChartButton = document.querySelector("#weekChart");
 
-weekChartButton.addEventListener('click', function(){
+weekChartButton.addEventListener('click', function()
+{
     var oldActive=document.querySelector('.itemTopActive');
-    if (oldActive){
+    if (oldActive)
+    {
         oldActive.classList.remove('itemTopActive');
     }
     weekChartButton.classList.add('itemTopActive');
-    console.log('clicked');
+
     clickRateChart.data.labels=["Week 42", "Week 43", "Week 44", "Week 45", "Week 46", "Week 47", "Week 48"];
     clickRateChart.data.datasets[0].data=[74, 125, 80, 78, 120, 115, 95];
     clickRateChart.update();
-})
+});
 
 var dayChartButton = document.querySelector("#dayChart");
-dayChartButton.addEventListener('click', function(){
+dayChartButton.addEventListener('click', function()
+{
     var oldActive=document.querySelector('.itemTopActive');
-    if (oldActive){
+    if (oldActive)
+    {
         oldActive.classList.remove('itemTopActive');
     }
     dayChartButton.classList.add('itemTopActive');
     clickRateChart.data.labels=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
     clickRateChart.data.datasets[0].data=[0, 10, 5, 25, 20, 30, 45];
     clickRateChart.update();
-})
+});
 
 var monthChartButton = document.querySelector("#monthChart");
-monthChartButton.addEventListener('click', function(){
+monthChartButton.addEventListener('click', function()
+{
     var oldActive=document.querySelector('.itemTopActive');
-    if (oldActive){
+    if (oldActive)
+    {
         oldActive.classList.remove('itemTopActive');
     }
     monthChartButton.classList.add('itemTopActive');
     clickRateChart.data.labels=["June", "July", "August", "September", "October", "November", "December"];
     clickRateChart.data.datasets[0].data=[200, 220, 210, 245, 260, 248, 235];
     clickRateChart.update();
-})
+});
 
 //SERIES CHART
 var ctx2 = document.getElementById('seriesChart').getContext('2d');
-var seriesChart = new Chart(ctx2, {
+var seriesChart = new Chart(ctx2, 
+{
     // The type of chart we want to create
     type: 'line',
 
     // The data for our dataset
-    data: {
+    data: 
+    {
         labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-        datasets: [{
+        datasets: 
+        [{
             label: "FX Weekly",
             borderColor: '#004977',
             backgroundColor:'#004977',
@@ -122,19 +137,21 @@ var seriesChart = new Chart(ctx2, {
             fill:0,
             lineTension:0.3,
             data: [108, 109, 145, 136, 127, 139, 158]
-        },
-        
-    ]
+        }]
     },
 
     // Configuration options go here
-    options: {
-        scales : {
-        xAxes : [ {
-            gridLines : {
-                display : false
-            }
-        } ]
-    }
+    options: 
+    {
+        scales : 
+        {
+            xAxes : 
+            [{
+                gridLines : 
+                {
+                    display : false
+                }
+            }]
+        }
     }
 });
